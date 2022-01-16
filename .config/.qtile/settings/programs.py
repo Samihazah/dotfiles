bc06eaff46e282e0b7513379669cf61e54e70544
Samihazah <62779151+Samihazah@users.noreply.github.com>
2022-01-16 21:31:35 +0100

    Add files via upload

---
+++ .config/.qtile/settings/programs.py
@@ -0,0 +1,14 @@
+# Default programs
+# Uncomment 1 per category
+# Requires restart
+
+# Terminal
+
+terminal = 'alacritty'
+
+# Browser
+
+browser = 'qutebrowser'
+#browser = 'firefox'
+
+
---
+++ .config/.qtile/settings/themes.py
@@ -0,0 +1,16 @@
+# THEMES
+# Requires restart
+# Change the theme variable in config.py to one of these:
+
+black = {
+        'foreground': '#FFFFFF',
+        'background': '#000000',
+        'fontshadow': '#010101'
+        }
+
+orange = {
+        'foreground': '#d75f00',
+        'background': '#303030',
+        'fontshadow': '#010101'
+        }
+
