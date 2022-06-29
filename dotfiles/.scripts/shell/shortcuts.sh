4a5fb7519a92da946bf0385af0d392916a4cbaa2
Samihazah <andrzejzkarpinski@gmail.com>
2022-06-29 14:47:45 +0200

    Revitalising dotfile folder

---
+++ dotfiles/.scripts/shell/shortcuts.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# This is a list of shortcuts to be sourced with bash/zsh.
+# Less vital than alias.sh, might be temporary.
+# Place in $SHELL directory.
+
+# Common user commands:
+alias vim='nvim'
+alias wiki='vim Documents/VimWiki/index.md'
+alias diary='vim Documents/VimWiki/diary/diary.md'
+alias py='python'
+alias tetra-venv='source ~/Documents/Python/Tetra/bin/activate'
+alias tetra-edit='vim ~/Documents/Python/Tetra'
+alias t3tr4='python ~/Documents/Python/Tetra/main.py'
+alias t3tr4-e='nvim ~/Documents/Python/Tetra/main.py'
+alias t3tr4-v='source ~/Documents/Python/Tetra/bin/activate && cd ~/Documents/Python/Tetra/'
+alias t3tr4-p='cd ~/Documents/Python/Tetra/'
+alias rling-nav='cd ~/Documents/Rust/rustlings'
+alias rling-edit='vim ~/Documents/Rust/rustlings/exercises'
+alias py='python'
+ 
